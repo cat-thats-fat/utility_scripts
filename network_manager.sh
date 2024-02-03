e23d2935e12f10ca7f9d2174322e29fa319bee70
Declan Bate-Smith <declanb@localhost.localdomain>
2024-02-03 16:28:21 -0500

    V1

---
+++ network_manager.sh
@@ -0,0 +1,8 @@
+echo "Taking adapter offline."
+ifconfig wlx347de44036ae down
+echo "Putting adapter into managed mode."
+iwconfig wlx347de44036ae mode managed
+echo "Taking adapter online."
+ifconfig wlx347de44036ae up
+echo "Restarting Network Manager service."
+service NetworkManager restart
